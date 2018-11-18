517575532808e402fd1135312ea6f4d1f89a5987
wen09 <44687798+wen09@users.noreply.github.com>
2018-11-18 17:46:20 -0500

    File IO
    Log and config are working

--- Config.cpp
+++ Config.cpp
@@ -6,26 +6,47 @@ extra ideas:
 
 /*
 -int max
--int dig123
+-int [] dig
 */
-#include <iostream>
-#include <math.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
-struct Config{
-	int max;
-	int dig1;
-	int dig2;
-	int dig3;
-};
-
-void loadConfig(Config& config){
+enum Error{SUCCESS, INPUT, OUTPUT, ARGUMENT, TIMEOUT};
 	
+void writeLog(FILE* log, time_t time, Error error, char detail []) {
+	fprintf(log, "%d, %d, %s", time, error, detail);
+	fflush(log);
 }
-
+		
 int main(){
+	time_t start = time(NULL);
+	FILE* log;
 	FILE* config;
+	
+	char input[1024];
+	Error error;
+	
+	log = fopen("/log", "a");
 	config = fopen("/data", "r");
-	max = 
+	
+	if(!log){
+		perror("can not open log");
+		return -1;
+	}
+	
+	if(!config){
+		perror("can not open config");
+		error = INPUT;
+		writeLog(log, start,error, "config file does not exist\n");
+		return -1;
+	}else{
+		fgets(input, 1024, config);
+		fclose(config);
+		error = SUCCESS;
+		writeLog(log, start, error, "input [] is initialized\n");
+	}
+	
+	return 0;
 }
