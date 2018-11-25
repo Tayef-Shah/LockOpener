ad800e922f83e8fa141012e27e140a48cf7d8509
Harry Zhang <zh.harry@outlook.com>
2018-11-25 16:51:24 -0500

    Web update

--- site/config.php
+++ site/config.php
@@ -1,5 +1,5 @@
-<form action="indx.php" method="get">
-	<input type="number" id="id" placeholder="0"></input>
+<form action="config.php" method="get">
+	<input type="number" id="id" value="0" placeholder="0"></input>
 	<input type="number" id="num1" placeholder="xx"></input>
 	<input type="number" id="num2" placeholder="xx"></input>
 	<input type="number" id="num3" placeholder="xx"></input>
--- site/index.php
+++ site/index.php
@@ -1,3 +1,4 @@
 <form action="indx.php" method="get">
+	<input type="number" value="0" placeholder="0"></input>
 	<input type="button" value="Open Lock"></input>
 </form>
