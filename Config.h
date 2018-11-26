d55b10e7df23b394c27747d733ebccb0535abc6c
wen09 <44687798+wen09@users.noreply.github.com>
2018-11-25 22:57:11 -0500

    nothing much

--- Config.h
+++ Config.h
@@ -5,30 +5,62 @@ extra ideas:
 */
 
 /*
-detail: function name, what it is
+detail: function name: what it is
 */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #include <stdio.h>
 #include <time.h>
 
 using namespace std;
 
+//types of errors
 enum Error{SUCCESS, INPUT, OUTPUT, ARGUMENT, TIMEOUT};
+
+//write to any file, takes in file name and what to write
+void writeFile(char name [], char word []){
+	FILE* fprt = fopen("/" + *name, "a");
 	
+	fprintf(fprt, "%s", word);
+	fclose(fprt);
+}
+
+//write to log, takes in time, type of error, and detail of log entry
 void writeLog(FILE* log, time_t time, Error error, char detail []) {
 	fprintf(log, "%d, %d, %s", time, error, detail);
-	fflush(log);
+	fclose(log);
 }
-		
-int startUp(){
-	time_t start = time(NULL);
+
+//read any file and returns what is being read
+char* readFile(char name []){
+	char input[1024];
+	
+	for(int i = 0; i < 1024; i++){
+		input[i] = 0;
+	}
+	FILE* fprt = fopen("/" + *name, "r");
+	if(fprt == NULL){
+		return NULL;
+	}
+	
+	fgets(input, 1024, fprt);
+	fclose(fprt);
+	return input;
+}
+
+//creats log and config, takes in time for log
+int startUp(time_t start){
+	//time_t start = time(NULL);
 	FILE* log;
 	FILE* config;
 	
 	char input[1024];
-	Error error;
 	
 	log = fopen("/log", "a");
 	config = fopen("/data", "r");
+	
 	
 	if(!log){
 		perror("can not open log");
@@ -37,15 +69,15 @@ int startUp(){
 	
 	if(!config){
 		perror("can not open config");
-		error = INPUT;
-		writeLog(log, start,error, "config file does not exist\n");
+		writeLog(log, start,INPUT, "config: file does not exist\n");
 		return -1;
 	}else{
 		fgets(input, 1024, config);
 		fclose(config);
-		error = SUCCESS;
-		writeLog(log, start, error, "input [] is initialized\n");
+		writeLog(log, start, SUCCESS, "config: input [] is initialized\n");
 	}
 	
 	return 0;
 }
+
+#endif //Config.h
