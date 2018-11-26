f62ded8efcdb6196d9f270aaaed8587c457b4851
wen09 <44687798+wen09@users.noreply.github.com>
2018-11-25 19:18:24 -0500

    Create Config.h

---
+++ Config.h
@@ -0,0 +1,51 @@
+/*
+extra ideas:
+-have multiple locks for config
+	-lock 1, lock 2 for user to choose
+*/
+
+/*
+detail: function name, what it is
+*/
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+enum Error{SUCCESS, INPUT, OUTPUT, ARGUMENT, TIMEOUT};
+	
+void writeLog(FILE* log, time_t time, Error error, char detail []) {
+	fprintf(log, "%d, %d, %s", time, error, detail);
+	fflush(log);
+}
+		
+int startUp(){
+	time_t start = time(NULL);
+	FILE* log;
+	FILE* config;
+	
+	char input[1024];
+	Error error;
+	
+	log = fopen("/log", "a");
+	config = fopen("/data", "r");
+	
+	if(!log){
+		perror("can not open log");
+		return -1;
+	}
+	
+	if(!config){
+		perror("can not open config");
+		error = INPUT;
+		writeLog(log, start,error, "config file does not exist\n");
+		return -1;
+	}else{
+		fgets(input, 1024, config);
+		fclose(config);
+		error = SUCCESS;
+		writeLog(log, start, error, "input [] is initialized\n");
+	}
+	
+	return 0;
+}
