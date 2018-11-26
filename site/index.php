d5dcd9aa6dab135226864d1cbe4c45dcd8f20562
Harry Zhang <zh.harry@outlook.com>
2018-11-26 13:00:07 -0500

    Site Polishing

--- site/index.php
+++ site/index.php
@@ -17,9 +17,71 @@ if (!empty($_GET)) {
 	}
 	$db->close();
 }
+
+// Grab Existing Locks
+$ids = array();
+
+$query = "SELECT id FROM data;";
+$select = $db->query($query);
+while ($row = $select->fetchArray(SQLITE3_ASSOC)) {
+	$ids[] = $row["id"];
+}
+
 ?>
 
-<form action="index.php" method="get">
-	<input type="number" required value="0" placeholder="0" name="id"></input>
-	<input type="submit" value="Open Lock"></input>
-</form>
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+		<title>Combination Lock Opener</title>
+	</head>
+	<body>
+		<div id="header">
+			<div id="leftItem">
+				Home
+			</div>
+			<div id="rightItem">
+				Config
+			</div>
+		</div>
+
+		<div id="body">
+			<form action="index.php" method="get">
+				<div class="form-group">
+					<label for="idSelect">Select Lock:</label>
+					<select class="form-control" id="idSelect">
+						<?php
+							foreach ($ids as $val) {
+								echo "<option>{$id}</option>";
+							}
+						?>
+					</select>
+				</div>
+				<input type="submit" value="Open Lock" class="btn btn-success"></input>
+			</form>
+		</div>
+
+		
+			<input type="number" required value="0" placeholder="0" name="id"></input>
+
+		</form>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<script src="script.js"></script>
+	</body>
+</html>
+
+<?php
+	$db->close();
+?>
