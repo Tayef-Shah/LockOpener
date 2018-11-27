02af6f0825e5ea2264a993c0c63e55029073e68f
Harry Zhang <zh.harry@outlook.com>
2018-11-27 09:43:11 -0500

    Reset

--- gpio/combo.h
+++ gpio/combo.h
@@ -6,7 +6,7 @@
 #include "stepper_motor.h"
 #include "servo_motor.h"
 
-const float LOCK_ADJ = 0.5;
+const float LOCK_ADJ = 0;
 
 int rotate(GPIO_Handle gpio, int ticks, int max){  //Argument is in the degrees of the lock
     int stepDegree = (int) ((512/(double)max) * (abs(ticks) + LOCK_ADJ));
