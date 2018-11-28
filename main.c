d3f51e0102fae609169c104b8cca1c917453f5d8
Harry Zhang <zh.harry@outlook.com>
2018-11-27 22:35:23 -0500

    Test

--- main.c
+++ main.c
@@ -108,7 +108,7 @@ int main() {
 	stepperInit(lockOpener.gpio);
 
 	// Program
-	/*while (1) {
+	while (1) {
 		//Initialize SQLite DB
 		lockOpener.zErrMsg = 0;
 		if (sqlite3_open(SQLITE_DB, &(lockOpener.db))) {
@@ -121,7 +121,7 @@ int main() {
 		}
 		sqlite3_close(lockOpener.db);
 		usleep(1000000);
-	}*/
+	}
 
 	/*rotate(lockOpener.gpio, 40, 40);
 	rotate(lockOpener.gpio, -40, 40);
@@ -137,7 +137,7 @@ int main() {
 	rotate(lockOpener.gpio, -40, 40);
 	stepperOff(lockOpener.gpio);*/
 
-	testStepper(lockOpener.gpio);
+	//testStepper(lockOpener.gpio);
     return 0;
 }
 
