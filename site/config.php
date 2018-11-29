be5cae3ddb488d3b31675d4761ec76182313a5f3
Harry Zhang <zh.harry@outlook.com>
2018-11-29 11:48:15 -0500

    negative num3

--- site/config.php
+++ site/config.php
@@ -8,6 +8,10 @@ if (!empty($_GET)) {
 	$num2 = $_GET["num2"] + 2;
 	$num3 = $_GET["num3"] - 1;
 
+	if ($num3 < 0) {
+		$num3 = 0;
+	}
+
 	$query = "
 		INSERT OR REPLACE INTO data (id, num1, num2, num3)
 		VALUES ({$id}, {$num1}, {$num2}, {$num3});
